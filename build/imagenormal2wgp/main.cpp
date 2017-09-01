fd5e405c4d4f2c8087a34460f759446a234616e4
tkuri <kuritateppei@gmail.com>
2017-09-01 16:08:36 +0900

    Fixed code.

--- build/imagenormal2wgp/main.cpp
+++ build/imagenormal2wgp/main.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <iomanip>
 #include <opencv2/opencv.hpp>
+#include <Misc/pixel_array.h>
+
+const float PI = 3.141592654f;
 
 float zenith2dopDiffuse(const float zenith, const float n = 1.6f){
 	float dop = sin(zenith) * sin(zenith) * (n - 1.0f / n) * (n - 1.0f / n);
@@ -14,21 +17,31 @@ float zenith2dopDiffuse(const float zenith, const float n = 1.6f){
 
 int convWGP2(const cv::Mat_<ushort>& img, const cv::Mat_<cv::Vec3w>& normal, cv::Mat_<ushort>& dst)
 {
-	for( int y = 0 ; y < img.rows ; y++ )
+	pol::PixelArray pixel_array(1, 11);
+
+	for (int y = 0; y < img.rows; y++)
 	{
 		for( int x = 0 ; x < img.cols ; x++ )
 		{
-			float nx = static_cast<float>(normal.at<cv::Vec3f>[2](y, x));
-			float ny = static_cast<float>(normal.at<cv::Vec3f>[1](y, x));
-			float nz = static_cast<float>(normal.at<cv::Vec3f>[0](y, x));
+			float c = static_cast<float>(img.at<ushort>(y, x));
+
+			float nx = static_cast<float>(normal.at<cv::Vec3w>(y, x)[2]) / 32767.0f - 32768.0f / 32767.0f;
+			float ny = static_cast<float>(normal.at<cv::Vec3w>(y, x)[1]) / 32767.0f - 32768.0f / 32767.0f;
+			float nz = static_cast<float>(normal.at<cv::Vec3w>(y, x)[0]) / 32767.0f - 32768.0f / 32767.0f;
 
 			float zenith = acos(nz);
 			float azimuth = atan2f(ny, nx);
-			if (azimuth < 0) azimuth += 360;
-			else if (azimuth >= 360) azimuth -= 360;
+			if (azimuth < 0) azimuth += 2*PI;
+			else if (azimuth >= 2*PI) azimuth -= 2*PI;
 
 			float dop = zenith2dopDiffuse(zenith);
+			float v[4];
+			v[0] = (c + dop*c*cos(2.0f*(0.0f / 180.0f * PI - azimuth))) / 2.0f;
+			v[1] = (c + dop*c*cos(2.0f*(45.0f / 180.0f * PI - azimuth))) / 2.0f;
+			v[2] = (c + dop*c*cos(2.0f*(90.0f / 180.0f * PI - azimuth))) / 2.0f;
+			v[3] = (c + dop*c*cos(2.0f*(135.0f / 180.0f * PI - azimuth))) / 2.0f;
 
+			dst.at<ushort>(y,x) = static_cast<ushort>(v[pixel_array.GetPolar(x, y)]);
 
 		}
 	}
@@ -37,13 +50,13 @@ int convWGP2(const cv::Mat_<ushort>& img, const cv::Mat_<cv::Vec3w>& normal, cv:
 }
 
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
 	std::string file;
 	cv::Mat img;
 	cv::Mat normal;
 
-	normal = cv::imread( argv[1],  CV_LOAD_IMAGE_ANYDEPTH | CV_LOAD_IMAGE_COLOR );
+	normal = cv::imread(argv[1], CV_LOAD_IMAGE_ANYDEPTH | CV_LOAD_IMAGE_COLOR);
 	img = cv::imread(argv[2], CV_LOAD_IMAGE_ANYDEPTH | CV_LOAD_IMAGE_COLOR);
 
 	if (img.cols != normal.cols || img.rows != normal.rows){
@@ -62,11 +75,16 @@ int main( int argc, char** argv )
 	}
 
 	if (img.channels() != 1){
-		std::cout << " Error : The number of image channel must be 1. ";
-		return -1;
+		cv::cvtColor(img, img, CV_RGB2GRAY);
 	}
 
-	cv::Mat_<ushort> dst( img.rows, img.cols);
+
+	cv::Mat_<ushort> dst(img.rows, img.cols);
+
+	if(0){
+		cv::Mat_<ushort> buf(img.rows, img.cols, 64*255);
+		img = buf.clone();
+	}
 
 	convWGP2(img, normal, dst);
 
