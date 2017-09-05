d396d096777755597c6125b15dbe43a8a57d213a
tkuri <kuritateppei@gmail.com>
2017-09-05 13:48:20 +0900

    replaced project.

--- build/merge_channels/main.cpp
+++ build/merge_channels/main.cpp
@@ -1,11 +1,8 @@
 
-#include <Windows.h>
-#include <vector>
 #include <fstream>
 #include <iostream>
-#include <iomanip>
 #include <opencv2/opencv.hpp>
-#include "Misc/FilenameParser.h"
+#include "misc/filename_parser.h"
 
 template<class T>
 int mergeChannels(const cv::Mat& src0, const cv::Mat& src1, const cv::Mat& src2, const cv::Mat& src3, cv::Mat& dst)
