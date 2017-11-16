93d4b9b162a56b55c37ed07b32e7af00080b2abd
tkuri <kuritateppei@gmail.com>
2017-11-16 15:36:31 +0900

    Implemented resize.

---
+++ build/resize/main.cpp
@@ -0,0 +1,41 @@
+
+#include <fstream>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+
+void usage(){
+	std::cout << "usage: " << std::endl;
+	std::cout << "xx.exe input resize_width resize_height output" << std::endl;
+}
+
+
+int main( int argc, char** argv )
+{
+	if (argc != 5){
+		usage();
+		return -1;
+	}
+
+	cv::Mat src = cv::imread(argv[1], CV_LOAD_IMAGE_ANYDEPTH | CV_LOAD_IMAGE_ANYCOLOR);
+	if (src.data == NULL){
+		std::cout << "input is invalid. " << std::endl;
+		return -1;
+	}
+	int width = atoi(argv[2]);
+	int height = atoi(argv[3]);
+	cv::Size size(width, height);
+
+	cv::Mat dst(size, src.type());
+
+	cv::resize(src, dst, size);
+	cv::imwrite(argv[4], dst);
+
+	std::cout << "Process finished. " << std::endl;
+//	std::cout << "Please return any key.. ";
+//	getchar();	
+
+	return 0;	
+}
+
