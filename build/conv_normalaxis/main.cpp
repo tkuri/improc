669341a58a1b1bb29c2697f1db229a0137a93ceb
tkuri <kuritateppei@gmail.com>
2018-03-29 11:36:09 +0900

    Modified conv_normalaxis.

--- build/conv_normalaxis/main.cpp
+++ build/conv_normalaxis/main.cpp
@@ -3,9 +3,19 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-
 typedef cv::Vec3f TNormal;     //! type of the normal vector
 static const int kCvTypeNormal = CV_MAKETYPE(cv::DataType<TNormal>::depth, 3); //! OpenCV type of the normal vector
+
+inline std::string ExtractPathWithoutExt(const std::string &fn)
+{
+    std::string::size_type pos;
+    if((pos = fn.find_last_of(".")) == std::string::npos){
+        return fn;
+    }
+ 
+    return fn.substr(0, pos);
+}
+
 
 int main( int argc, char** argv )
 {
@@ -13,52 +23,63 @@ int main( int argc, char** argv )
 	cv::Mat img;
 	cv::Mat dst;
 
-	file = argv[1];
-	img = cv::imread( file.c_str(),  CV_LOAD_IMAGE_ANYDEPTH | CV_LOAD_IMAGE_COLOR );
 
-	std::cout << "normal : " << argv[1] << std::endl;
+	for(int n=1; n<argc; n++){
+		file = argv[n];
+		img = cv::imread( file.c_str(),  CV_LOAD_IMAGE_ANYDEPTH | CV_LOAD_IMAGE_COLOR );
 
-	int width = img.cols;
-	int height = img.rows;
-	int dep = img.depth();
-	int ch = img.channels();
+		std::cout << "normal : " << argv[1] << std::endl;
 
-	dst = cv::Mat( height, width, img.type() );
+		int width = img.cols;
+		int height = img.rows;
+		int dep = img.depth();
+		int ch = img.channels();
 
-	if( dep == 0 && ch == 3 ){
-		 std::vector<cv::Mat> planes;
-		 cv::split(img, planes);
-		 cv::Mat xaxis = planes[2].clone();
-		 cv::Mat yaxis = planes[1].clone();
-		 cv::Mat zaxis = planes[0].clone();
+		dst = cv::Mat( height, width, img.type() );
 
-		 planes[2] = xaxis; //x
-		 planes[1] = zaxis; //y
-		 planes[0] = 255 - yaxis; //z
+		if( dep == 0 && ch == 3 ){
+			 std::vector<cv::Mat> planes;
+			 cv::split(img, planes);
+			 cv::Mat xaxis = planes[2].clone();
+			 cv::Mat yaxis = planes[1].clone();
+			 cv::Mat zaxis = planes[0].clone();
 
-		 cv::merge(planes, dst);
+			 //planes[2] = xaxis; //x
+			 //planes[1] = zaxis; //y
+			 //planes[0] = 255 - yaxis; //z
+			 planes[2] = 255 - xaxis; //x
+			 planes[1] = yaxis; //y
+			 planes[0] = zaxis; //z
+
+			 cv::merge(planes, dst);
+		}
+		else if( dep == 2 && ch == 3 ){
+			 std::vector<cv::Mat> planes;
+			 cv::split(img, planes);
+			 cv::Mat xaxis = planes[2].clone();
+			 cv::Mat yaxis = planes[1].clone();
+			 cv::Mat zaxis = planes[0].clone();
+
+			 //planes[2] = xaxis; //x
+			 //planes[1] = zaxis; //y
+			 //planes[0] = 65535 - yaxis; //z
+			 planes[2] = 65535 - xaxis; //x
+			 planes[1] = yaxis; //y
+			 planes[0] = zaxis; //z
+
+			 cv::merge(planes, dst);
+		}
+		else
+		{
+			std::cout << " Error : Invalid depth or channels. ";
+			return -1;
+		}
+		std::string ofile = ExtractPathWithoutExt(file);
+		ofile += "_conv.png";
+
+		cv::imwrite( ofile.c_str(), dst);
 	}
-	else if( dep == 2 && ch == 3 ){
-		 std::vector<cv::Mat> planes;
-		 cv::split(img, planes);
-		 cv::Mat xaxis = planes[2].clone();
-		 cv::Mat yaxis = planes[1].clone();
-		 cv::Mat zaxis = planes[0].clone();
 
-		 planes[2] = xaxis; //x
-		 planes[1] = zaxis; //y
-		 planes[0] = 65535 - yaxis; //z
-
-		 cv::merge(planes, dst);
-	}
-	else
-	{
-		std::cout << " Error : Invalid depth or channels. ";
-		return -1;
-	}
-
-
-	cv::imwrite( "convAxisNormal.png", dst );
 
 	std::cout << "Convert finished. ";
 //	std::cout << "Please return any key.. ";
