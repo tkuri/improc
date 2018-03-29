c7be91062aaad73f14b14d269a7b424ffcf23315
tkuri <kuritateppei@gmail.com>
2018-03-29 12:34:56 +0900

    Added png2bmp.

---
+++ build/png2bmp/main.cpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include "misc/filename_parser.h"
+
+int main(int argc, char** argv)
+{
+	if(argc < 2) return 1;
+
+	for(int n=1; n<argc; n++){
+
+		if(FilenameParser::getExtension(argv[n]) == "png"){
+
+			cv::Mat image = cv::imread(argv[n], 6);
+			if(image.data == NULL) break;
+			if(image.channels()==1){
+				cv::Mat buf;
+				cv::cvtColor(image, buf, cv::COLOR_GRAY2BGR);
+				image = buf.clone();
+			}
+				
+			unsigned short width = image.cols;
+			unsigned short height = image.rows;
+
+			std::string filename = FilenameParser::removeExtension(argv[n]) + ".bmp";
+			std::cout << filename << std::endl;
+
+			cv::imwrite(filename, image);
+
+		}
+		else if(FilenameParser::getExtension(argv[n]) == "bmp"){
+
+			cv::Mat image = cv::imread(argv[n], 6);
+			if(image.data == NULL) break;
+			if(image.channels()==1){
+				cv::Mat buf;
+				cv::cvtColor(image, buf, cv::COLOR_GRAY2BGR);
+				image = buf.clone();
+			}
+				
+			unsigned short width = image.cols;
+			unsigned short height = image.rows;
+
+			std::string filename = FilenameParser::removeExtension(argv[n]) + ".png";
+			std::cout << filename << std::endl;
+
+			cv::imwrite(filename, image);
+
+		}
+		else{
+			std::cout << "Invalid file format" << std::endl;
+		}
+	}
+
+	return 0;
+}
